20a2e5362b40d9971c1e8a4df054818d43f28ba8
Ma1kaveli <74207027+Ma1kaveli@users.noreply.github.com>
2021-02-09 19:08:13 +0500

    Add files via upload

---
+++ Лабораторная работа 2/js/app.js
@@ -0,0 +1,94 @@
+//#region Калькулятор
+//Создаю стрелочную функцию, которая принимает параметр и выводит в imput
+const insert = num => document.form.textview.value = document.form.textview.value + num 
+//Очищаем строку полностью
+const clean = () => document.form.textview.value = ''
+//Вычитаем последний символ
+const minusLastSymbol = () => {
+    var string = document.form.textview.value
+    document.form.textview.value = string.substring(0, string.length - 1)
+}
+//Функция вычисления(eval сделает все сама)
+function result(){
+    var string = document.form.textview.value
+    if(string){
+        document.form.textview.value = eval(string)
+    }
+}
+//#endregion
+
+//#region Чеклист
+
+//block - это div
+let block = document.querySelectorAll('.check')
+//text - это текст который находится в строке
+let text = document.getElementsByTagName('span')
+
+//line - это input в который пишут текст
+const line = document.querySelector('[maxLength="30"]')
+//checklist - в нем находятся и создаются задачи
+const checklist = document.querySelector('[class="checklist"]')
+
+let tcheckbox = document.querySelector('[type="checkbox"]')
+
+let count = 5
+let counts = 5
+
+
+//Если Enter, запускает enterClick
+line.addEventListener('keydown', function(event){
+    if (event.keyCode === 13){
+        enterClick(line.value)
+        line.value = ''
+    }
+})
+
+function enterClick (line){
+    let a = count
+    let b = counts
+    var div = document.createElement('div')
+    var p = document.createElement('p')
+    var input = document.createElement('input')
+    var span = document.createElement('span')
+    var img = document.createElement('img')
+
+    //Добавление свойств к тегам
+    div.className = 'list check'
+    input.type = "checkbox"
+    input.name = "check"
+    span.innerHTML = line
+    img.src = "img/x.png"
+    img.alt = "img"
+
+    //То в каком порядке должны находиться теги
+    checklist.appendChild(div)
+    div.appendChild(p)
+    p.appendChild(input)
+    p.appendChild(span)
+    p.appendChild(img)
+
+    input.onclick = checkboxClick(a)
+    img.onlclick = deleteString(b)
+
+    count++
+    counts++
+
+    console.log(count)
+    console.log(counts)
+}
+
+//Скрывает чекбокс и меняет стиль текста
+const checkboxClick = number => {
+    tcheckBox[number].hidden = true
+    text[number].style.color = 'grey'
+    text[number].style.textDecoration = 'line-through'
+    text[number].style.marginLeft = '26px'
+}
+
+//Удаление одного div
+
+const deleteString = n => {
+    block[n].hidden = true
+}
+//#endregion
+
